e7323232394bd53dbe6fc87a1cb6176e051f9ef3
akhilggp <akhilgdp@gmail.com>
2024-09-24 23:18:09 -0700

    NIT Changes

--- src/main/java/com/example/SmartLock/controller/UserController.java
+++ src/main/java/com/example/SmartLock/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.example.SmartLock.controller;
 
-import com.example.SmartLock.model.User;
 import com.example.SmartLock.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
